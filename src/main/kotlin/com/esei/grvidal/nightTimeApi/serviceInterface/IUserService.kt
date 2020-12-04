402eccc52a80a88960642fd3b2124935ffa8dd79
minimerlyn <gabrielrv98@gmail.com>
2020-12-04 19:20:53 +0100

    Updates the @Throws

--- src/main/kotlin/com/esei/grvidal/nightTimeApi/serviceInterface/IUserService.kt
+++ src/main/kotlin/com/esei/grvidal/nightTimeApi/serviceInterface/IUserService.kt
@@ -1,8 +1,10 @@
 package com.esei.grvidal.nightTimeApi.serviceInterface
 
+import com.esei.grvidal.nightTimeApi.exception.NotFoundException
 import com.esei.grvidal.nightTimeApi.model.User
 import com.esei.grvidal.nightTimeApi.projections.UserProjection
 import java.util.*
+import kotlin.jvm.Throws
 
 /**
  * DAO Interface for Bars
@@ -13,17 +15,21 @@ interface IUserService {
     fun list(): List<UserProjection>
 
     //Show one user
+    @Throws(NotFoundException::class)
     fun loadProjection(idUser: Long): UserProjection
 
     //Show one user
+    @Throws(NotFoundException::class)
     fun load(idUser: Long): User
 
     //Save a new user
     fun save(user: User): User
 
     //remove an user
+    @Throws(NotFoundException::class)
     fun remove(idUser: Long)
 
+    @Throws(NotFoundException::class)
     fun login(nickname: String, password: String): Boolean
 
 }
