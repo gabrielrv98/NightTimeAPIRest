526747029890cbc20fe7b29f95d66238c58b73b2
minimerlyn <gabrielrv98@gmail.com>
2020-12-06 18:37:09 +0100

    changes nullable injection for lateinit

--- src/main/kotlin/com/esei/grvidal/nightTimeApi/services/DateCityService.kt
+++ src/main/kotlin/com/esei/grvidal/nightTimeApi/services/DateCityService.kt
@@ -1,7 +1,11 @@
 package com.esei.grvidal.nightTimeApi.services
 
+import com.esei.grvidal.nightTimeApi.dto.DateCityDTO
+import com.esei.grvidal.nightTimeApi.exception.AlreadyExistsException
+import com.esei.grvidal.nightTimeApi.exception.NotFoundException
 import com.esei.grvidal.nightTimeApi.repository.DateCityRepository
 import com.esei.grvidal.nightTimeApi.exception.ServiceException
+import com.esei.grvidal.nightTimeApi.model.City
 import com.esei.grvidal.nightTimeApi.model.DateCity
 import com.esei.grvidal.nightTimeApi.serviceInterface.IDateCityService
 import org.springframework.beans.factory.annotation.Autowired
@@ -22,7 +26,7 @@ class DateCityService : IDateCityService {
 
 
     @Autowired
-    val dateCityRepository: DateCityRepository? = null
+    lateinit var dateCityRepository: DateCityRepository
 
 
     /**
@@ -32,7 +36,7 @@ class DateCityService : IDateCityService {
     override fun list(): List<DateCity> {
 
         try {
-            return dateCityRepository!!.findAll()
+            return dateCityRepository.findAll()
         } catch (e: Exception) {
             throw ServiceException(e.message)
         }
@@ -42,12 +46,14 @@ class DateCityService : IDateCityService {
     override fun getTotalPeopleByDateAndCity(nextCity_id: Long, nextDate: LocalDate): Int {
         try {
 
-            return dateCityRepository!!.countAllByNextCity_IdAndNextDate(nextCity_id, nextDate)
+            return dateCityRepository.countAllByNextCity_IdAndNextDate(nextCity_id, nextDate)
         } catch (e: Exception) {
             throw ServiceException(e.message)
         }
 
     }
+
+
 
     /**
      * This will save a new bar, if not, will throw an Exception
@@ -56,12 +62,11 @@ class DateCityService : IDateCityService {
     override fun save(dateCity: DateCity): DateCity {
 
         try {
-            return dateCityRepository!!.save(dateCity)
+            return dateCityRepository.save(dateCity)
         } catch (e: Exception) {
             throw ServiceException(e.message)
         }
     }
-
 
 }
 
