2472209a8efb0dd7a2b56cfca575e5e31fde2f78
Gabriel Rguez Vidal <gabrielrv98@gmail.com>
2021-01-15 04:46:44 +0100

    Adds future work

--- src/main/kotlin/com/esei/grvidal/nightTimeApi/utlis/AnswerOptions.kt
+++ src/main/kotlin/com/esei/grvidal/nightTimeApi/utlis/AnswerOptions.kt
@@ -1,6 +1,10 @@
 package com.esei.grvidal.nightTimeApi.utlis
 
-enum class AnswerOptions(){
+/**
+ * Enum with the option to answer a friendship request
+ * TODO add a 4th state "ERASED", where users can't send messages on the chat but they can read the already written chat
+ */
+enum class AnswerOptions {
     NOT_ANSWERED,
     YES,
     NO
