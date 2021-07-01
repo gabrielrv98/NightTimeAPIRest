38f8e7e6b37df31808b7d5843ae04a77868024a3
Gabriel Rguez Vidal <gabrielrv98@gmail.com>
2021-07-02 01:53:29 +0200

    Improves projection

--- src/main/kotlin/com/esei/grvidal/nightTimeApi/projections/EventProjection.kt
+++ src/main/kotlin/com/esei/grvidal/nightTimeApi/projections/EventProjection.kt
@@ -1,10 +1,12 @@
 package com.esei.grvidal.nightTimeApi.projections
 
+import org.springframework.beans.factory.annotation.Value
 import java.time.LocalDate
 
 interface EventProjection {
     fun getId(): Long
     fun getDescription() : String
     fun getDate() : LocalDate?
+    @Value("#{target.bar.name}")
     fun getBarName(): String
 }
